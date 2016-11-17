c8cf6d97f0adbfc2b12b4cf379ebc965e325a57f
Jesús Edel Cereceres Delgado <jesus.edelcereceres@gmail.com>
2016-11-17 17:43:44 +0000

    the readme is updated

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 'use strict'
 let pdf = require("../index")
+let plot = require('plot-distribution')
 let assert = require("assert")
-describe('the test to uid', function () {
+describe('the test to pdf', function () {
     it('should be a function', function () {
         assert(typeof pdf === 'function')
     })
     
     it('should return a number', function () {
+        console.log(plot(pdf,{min:-2,max:2, step: 0.1, points: 10000}))
         assert(typeof pdf() === 'number')
     })
 })
